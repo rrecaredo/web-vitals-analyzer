570b2145eb0a7a2640071fc016737f7864ed636f
Ricardo Recaredo <ricardo.barca@wayflyer.com>
2023-10-28 11:15:16 +0200

    move secrets to a config file

--- api/infrastructure/api-proxy.ts
+++ api/infrastructure/api-proxy.ts
@@ -1,21 +1,16 @@
-// @TODO: Idieally this should be in ENVIROMENT variable, or Vault secret.
-// eslint-disable-next-line no-secrets/no-secrets
-const USERNAME = "FL35VZJ447mATGHXTiBdX4m2rIQfvrNB";
-
-const BASE_API_URL =
-  "https://api-4b0379cd-b47bd823-dku.us-east-1.app.dataiku.io/public/api/v1/test-api/";
+import env from "./environment";
 
 export async function getFromExternalApi<T>(url: string, data: T) {
   const options = {
     method: "POST", // Both external endpoints are POST requests so no need to make it configurable for now
     headers: {
-      Authorization: "Basic " + Buffer.from(USERNAME + ":").toString("base64"),
+      Authorization: "Basic " + Buffer.from(env.USERNAME + ":").toString("base64"),
       "Content-Type": "application/json",
     },
     body: JSON.stringify(data),
   };
 
-  const response = await fetch(`${BASE_API_URL}${url}`, options);
+  const response = await fetch(`${env.BASE_API_URL}${url}`, options);
   const allData = await response.json();
   const parsedData = JSON.parse(allData.response);
 
---
+++ api/infrastructure/environment.ts
@@ -0,0 +1,9 @@
+/* eslint-disable no-secrets/no-secrets */
+
+/**
+ * This should be an Environment variable, or Vault secret.
+ */
+export default {
+  USERNAME: "",
+  BASE_API_URL: "",
+};
